e7f9ccdd0fba87bdebee4e7712ebefd001ce5b7f
Dmitry Birin <dmitry.birin@gmail.com>
2018-02-02 10:42:19 +0300

    WIP: analyzer

--- analyze.py
+++ analyze.py
@@ -1,6 +1,37 @@
 import csv
+import re
+from datetime import datetime
+OPEN_REGEXP = r'В ([\d]{2})-([\d+]{2}) (вестибюль [1-2] ст.|ст.) ([а-я А-Я-12]+) открыт[аы]*[,.]{1} ([а-я А-Я]+)'
+CLOSED_REGEXP = r'В ([\d]{2})-([\d+]{2}) (вестибюль [1-2] ст.|ст.) ([а-я А-Я-12]+) закрыт[аы]* (из-за [а-я А-Я]+)'
+
+data = []
+event = dict()
 
 with open(file='./imported_data.csv', mode='r', encoding='utf-8-sig') as csv_file:
     reader = csv.DictReader(csv_file)
     for row in reader:
-        print(row)
+        event['raw_message'] = row['Raw Message']
+        event['message_timestamp'] = datetime.strptime(row['Date'], '%Y-%m-%d %H:%M:%S')
+        # need only date?
+        #event['date'] = 
+        event['view_count'] = row['View Count']
+        open_result = re.match(OPEN_REGEXP, row['Raw Message'])
+        closed_result = re.match(CLOSED_REGEXP, row['Raw Message'])
+        if open_result is not None:
+            results = open_result.groups()
+            event['time'] = datetime(hour=results[0], minute=results[1])
+            event['station'] = results[3]
+            event['reason'] = results[4]
+            event['state'] = 'open'
+        elif closed_result is not None:
+            results = closed_result.groups()
+            event['time'] = datetime(hour=results[0], minute=results[1])
+            event['station'] = results[3]
+            event['reason'] = results[4]
+            event['state'] = 'closed'
+        else:
+            event['state'] = 'unknown'
+
+        data.append(event)
+        
+print(data)
