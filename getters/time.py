833037c2d2020ebd89c2c62dcec33a099101f191
Dmitry Birin <dmitry.birin@gmail.com>
2018-02-03 21:49:36 +0300

    time bug with match/search fixed

--- getters/time.py
+++ getters/time.py
@@ -1,10 +1,10 @@
 import re
 from datetime import time
 
-TIME_REGEXP = r'([\d]{2})-([\d+]{2})'
+TIME_REGEXP = r"([\d]{2})[-.]([\d]{2})"
     
 def get_time_from_text(text):
-    time_result = re.match(TIME_REGEXP, text)
+    time_result = re.search(TIME_REGEXP, text)
     if time_result is not None:
         results = time_result.groups()
         return time(int(results[0]), int(results[1]))            
